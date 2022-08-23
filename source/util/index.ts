80609b19dffa968911b2a66279efe780a16622c7
Jeff Eaton <jeff@eaton.fyi>
2022-08-23 10:30:44 -0500

    Consolidating MIME handling functions

---
+++ source/util/index.ts
@@ -0,0 +1,19 @@
+export { JsonObject, JsonValue } from 'type-fest';
+export { Dictionary } from '@autogram/autograph';
+export {
+  ParsedUrl,
+  NormalizedUrl,
+  ParsedUrlSet,
+  NormalizedUrlSet,
+  UrlFilters,
+  UrlMutators,
+} from '@autogram/url-tools';
+export {
+  Filter,
+  Mutator,
+  Extractor,
+  FilterSet,
+  MutatorSet,
+  ExtractorSet,
+  INTERVALS
+} from './types.js';
--- source/util/mime.ts
+++ source/util/mime.ts
@@ -1,2 +1,18 @@
+import is from '@sindresorhus/is';
 import MIMEType from 'whatwg-mimetype';
 import mime from 'mime';
+
+export class Mime {
+  static parse = (type: string) : MIMEType | undefined => {
+    const parsed = MIMEType.parse(type);
+    if (!is.null_(parsed)) return parsed;
+  }
+
+  static extensionFromType = (type: string): string | undefined => {
+    return mime.getExtension(type) ?? undefined;
+  }
+
+  static typeFromExtension = (extension: string): string | undefined => {
+    return mime.getType(extension) ?? undefined;
+  }
+}
