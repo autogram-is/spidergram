eaeb8b0acf52b99babb1acf46fcef430ff7e2d19
Jeff Eaton <jeff@eaton.fyi>
2022-08-28 15:02:50 -0500

    Exposing defaultFetchOptions

--- source/fetch/index.ts
+++ source/fetch/index.ts
@@ -1,4 +1,4 @@
-export { Fetcher, FetchOptions, FetchRules } from './fetcher.js';
+export { Fetcher, FetchOptions, FetchRules, defaultFetchOptions } from './fetcher.js';
 export { GotFetcher } from './got-fetcher.js';
 export * as BROWSER_PRESETS from './browser-presets.js';
 export * as ResponseFilters from './response-filters.js';
