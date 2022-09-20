5cddde25753ad2a0c63b2935f9d188df4930d4b7
Jeff Eaton <jeff@eaton.fyi>
2022-09-20 13:42:49 -0500

    Extract all meta tags

--- source/extract/metadata-from-resource.ts
+++ source/extract/metadata-from-resource.ts
@@ -1,18 +1,71 @@
 import is from '@sindresorhus/is';
+import { getProperty, setProperty, hasProperty, deepKeys } from 'dot-prop';
+
 import { CheerioParser } from './cheerio.js';
 import { Dictionary, Resource } from '../index.js';
 import { isHtml } from '../fetch/response-filters.js';
 
-export function metadataFromResource(resource: Resource, customSelectors: Dictionary<string> = {}): Dictionary<string> {
-  const results: Dictionary<string> = {};
+export type Properties<T = string | number | boolean > = { [property: string]: Property<T> };
+export type Property<T = string | number | boolean > =
+  | T
+  | Property[]
+  | Properties
+  | undefined
+
+export function metadataFromResource(resource: Resource): Properties {
+  const results: Properties = {};
   if (isHtml(resource) && is.nonEmptyStringAndNotWhitespace(resource.body)) {
     const $ = new CheerioParser(resource.body).root;
-    results.title = $('title').text();
-    results.description = $('meta[description]').attr('content');
 
-    for (let k in customSelectors) {
-      results[k] = $(customSelectors[k]).text();
-    }  
+    const meta = $('head meta');
+    meta.each((index, element) => {
+      const key = $(element).attr('name')?.trim() ?? $(element).attr('property')?.trim();
+      const value = $(element).attr('content')?.trim();
+      if (key) {
+        if (key == 'description') {
+          setProp(results, key, value)
+        } else {
+          appendProperty(results, key, value);
+        }
+      }
+    })
+
+    results.title = getProperty(results, 'og.title', $('head title').text().toString());
   }
   return results;
 }
+
+function setProp(
+  object: Properties,
+  key: string,
+  value: Property,
+  keyDelimiter: string = ':'
+  ) {
+  const path = key.replace(keyDelimiter, '.');
+  setProperty(object, path, value);
+}
+
+function appendProperty(
+  object: Properties,
+  key: string,
+  value: Property,
+  keyDelimiter: string = ':'
+  ) {
+  const path = key.replace(keyDelimiter, '.');
+  const currentProp = getProperty(object, path);
+
+
+  if (is.undefined(currentProp)) {
+    setProperty(object, path, value);
+  } else if (is.array(currentProp)) {
+    if (is.array(value)) {
+      setProperty(object, path, [...currentProp, ...value]);
+    }
+    else {
+      setProperty(object, path, [...currentProp, value]);
+    }
+  } else {
+    setProperty(object, path, [currentProp, value]);
+  }
+}
+
