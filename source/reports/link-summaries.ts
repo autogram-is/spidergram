b405da385bba490f2486e317b5fcb2527f88bbf4
Jeff Eaton <jeff@eaton.fyi>
2022-10-08 00:26:12 -0500

    Well, what do you know. Even reporting works.

---
+++ source/reports/link-summaries.ts
@@ -0,0 +1,116 @@
+import { aql } from 'arangojs';
+
+export class LinkSummaries {
+
+  static inboundLinkCount = aql`
+    let inlinks = (
+      for lt in links_to
+        filter lt._to == url._id
+      return distinct lt._from
+    )
+  `;
+
+  static pages() {
+    return aql`
+      for resource in resources
+        filter resource.code == 200
+      for rw in responds_with
+        filter rw._to == resource._id
+      for url in unique_urls
+        filter url._id == rw._from
+
+      ${LinkSummaries.inboundLinkCount}
+
+      return {
+        host: url.parsed.host,
+        path: url.parsed.pathname,
+        title: resource.metadata.title,
+        sentences: resource.readability.sentenceCount,
+        fog: resource.readability.gunningFog,
+        references: length(inlinks),
+        size: resource.headers.´content-length´,
+      }
+  `}
+
+  static downloads() {
+    return aql`
+      for resource in resources
+        filter resource.code == 200
+        filter resource.headers.´content-type´ NOT LIKE '%html%'
+      for rw in responds_with
+        filter rw._to == resource._id
+      for url in unique_urls
+        filter url._id == rw._from
+
+      ${LinkSummaries.inboundLinkCount}
+
+      return {
+        host: url.parsed.host,
+        path: url.parsed.pathname,
+        references: length(inlinks),
+        size: resource.headers.´content-length´,
+        mime: resource.headers.´content-type´,
+      }
+  `}
+
+  static errors() {
+    return aql`
+      for resource in resources
+        filter resource.code != 200
+      for rw in responds_with
+        filter rw._to == resource._id
+      for url in unique_urls
+        filter url._id == rw._from
+
+      ${LinkSummaries.inboundLinkCount}
+
+      return {
+        host: url.parsed.host,
+        path: url.parsed.pathname,
+        code: resource.code,
+        message: resource.message,
+        references: length(inlinks)
+      }
+  `}
+
+
+  static malformed() {
+    return aql`
+      for url in unique_urls
+      filter url.parsed == null
+
+      ${LinkSummaries.inboundLinkCount}
+
+      return {
+        url: url.url,
+        references: length(inlinks)
+      }
+  `}
+
+  static excludeProtocol(excluded: string[] = ['https:', 'http:']) {
+    return aql`
+      for url in unique_urls
+      filter url.parsed.protocol NOT IN ${excluded}
+
+      ${LinkSummaries.inboundLinkCount}
+
+      return {
+        url: url.url,
+        references: length(inlinks)
+      }
+  `}
+
+  static outlinks(internalDomains: string[]) {
+    return aql`
+      for url in unique_urls
+      filter url.parsed.domain NOT IN ${internalDomains}
+
+      ${LinkSummaries.inboundLinkCount}
+      
+      return {
+        host: url.parsed.host,
+        path: url.parsed.pathname,
+        references: length(inlinks)
+      }
+  `}
+}
--- tests/fixtures/scratch.ts
+++ tests/fixtures/scratch.ts
@@ -1,34 +1,82 @@
 import { Arango, aql } from '../../source/arango.js';
 import { CheerioSpider } from '../../source/spider/cheerio/cheerio-spider.js';
 import { ProcessOptions, processResources } from '../../source/analysis/index.js';
+import { JsonObject } from '../../source/types.js';
 
+// Assorted parsing helpers
 import { getMeta } from '../../source/analysis/index.js';
 import { htmlToText } from 'html-to-text';
 import readability from 'readability-scores';
 
-// Set up the Arango connection
+// Sheets.js setup
+import * as XLSX from 'xlsx';
+import * as fs from 'fs';
+XLSX.set_fs(fs);
+import { Readable } from 'stream';
+XLSX.stream.set_readable(Readable);
+import { LinkSummaries } from '../../source/reports/link-summaries.js';
+import { AqlQuery } from 'arangojs/aql.js';
+
+const targetDomain = 'autogram.is';
 const a = new Arango();
-await a.load('example');
+await a.load(targetDomain.replace('.', '_'));
 
-// Create the spider instance with our settings
-const spider = new CheerioSpider({
-  storage: a,
-  autoscaledPoolOptions: {
-    maxConcurrency: 5,
-    maxTasksPerMinute: 240
-  }
-});
+await crawl();
+await process();
+await report();
 
-// Populate the initial URL list and run the crawl
-console.log(await spider.run(['https://uscis.gov']));
-
-// Now loop over the results we saved and extract more data
-const filter = aql`FILTER resource.body != null`;
-
-const options:ProcessOptions = {
-  metadata: resource => (resource.body) ? getMeta(resource.body) : undefined,
-  text: resource => (resource.body) ? htmlToText(resource.body) : undefined,
-  readability: resource => (resource.text) ? readability(resource.text as string) : undefined,
+/**
+ * Crawl a URL and all of its linked pages
+ */
+export async function crawl() {
+  const spider = new CheerioSpider({
+    storage: a,
+    autoscaledPoolOptions: {
+      maxConcurrency: 5,
+      maxTasksPerMinute: 360
+    }
+  });
+  const crawlResults = await spider.run([`https://${targetDomain}`]);
+  console.log(crawlResults);
+  return Promise.resolve();
 }
 
-console.log(await processResources(a, filter, options));
+/**
+ * Iterate over the results and extract useful information
+ */
+
+export async function process() {
+  const filter = aql`FILTER resource.body != null`;
+  const options:ProcessOptions = {
+    metadata: resource => (resource.body) ? getMeta(resource.body) : undefined,
+    text: resource => (resource.body) ? htmlToText(resource.body, { 
+      baseElements: { selectors: ['main'] }
+    }) : undefined,
+    readability: resource => (resource.text) ? readability(resource.text as string) : undefined,
+  }
+  const processResults = await processResources(a, filter, options);
+  console.log(processResults.errors ?? 'All records processed successfully');
+  return Promise.resolve();
+}
+
+/**
+ * Generate a spreadsheet with some useful reports
+ */
+export async function report() {
+  const queries: Record<string, AqlQuery> = {
+    'Pages': LinkSummaries.pages(),
+    'Errors': LinkSummaries.errors(),
+    'Malformed URLs': LinkSummaries.malformed(),
+    'Non-Web URLs': LinkSummaries.excludeProtocol(),
+    'External Links': LinkSummaries.outlinks([targetDomain])
+  };
+  const workbook = XLSX.utils.book_new();
+  for (let key in queries) {
+    const cursor = await a.db.query(queries[key]);
+    const result = (await cursor.all()).map(value => value as JsonObject);
+    XLSX.utils.book_append_sheet(workbook, XLSX.utils.json_to_sheet(result), key);
+  }
+  XLSX.writeFileXLSX(workbook, `storage/${targetDomain}.xlsx`);
+  console.log(`storage/${targetDomain}.xlsx generated.`);
+  return Promise.resolve();
+}
