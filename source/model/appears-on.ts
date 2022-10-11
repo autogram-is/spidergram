8109214a278a1e5c3513903d10c007261cc543d3
Jeff Eaton <jeff@eaton.fyi>
2022-10-10 19:48:13 -0500

    Adding the AppearsOn edge type

---
+++ source/model/appears-on.ts
@@ -0,0 +1,27 @@
+import { Resource } from './resource.js';
+import { Edge, EdgeData } from './edge.js';
+import { Vertice, Reference } from './vertice.js';
+
+export type AppearsOnData<F extends Vertice = Vertice, T extends Vertice = Resource> = EdgeData<F, T> & {
+  item?: Reference<F>
+  location?: Reference<T>
+  context?: string
+};
+
+export class AppearsOn<F extends Vertice = Vertice, T extends Vertice = Resource> extends Edge<F, T> {
+  override readonly _collection = 'appears_on';
+  context!: string
+
+  constructor(data: AppearsOnData<F, T> = {}) {
+    const { item, location, context, ...dataForSuper } = data;
+    
+    dataForSuper.from ??= item;
+    dataForSuper.to ??= location;
+
+    super(dataForSuper);
+
+    this.context = context ?? 'pattern';
+  }
+}
+
+Vertice.types.set('appears_on', { constructor: AppearsOn, isEdge: true });
