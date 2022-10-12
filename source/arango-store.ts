99ebd68607751841fb40f4635556dc88063f3bb3
Jeff Eaton <jeff@eaton.fyi>
2022-10-12 12:33:22 -0500

    ArangoStore instance

--- source/arango-store.ts
+++ source/arango-store.ts
@@ -12,28 +12,12 @@ import { assert } from "console";
 // await Arango.add/set/etc operates on the database itself
 export class ArangoStore {
   protected static _systemDb?: Database;
-  protected static _activeDb?: Database;
-
-  protected constructor(options: Partial<Config> = {}) {
-    options.url ??= 'http://127.0.0.1:8529';
-    ArangoStore._systemDb = new Database(options);
-  }
-
-  /**
-   * The current active Arango database.
-   */
-  static get db(): Database {
-    if (is.undefined(ArangoStore._activeDb)) {
-      throw new Error('No working database loaded');
-    } else {
-      return ArangoStore._activeDb!;
-    }
-  }
+  constructor(protected _activeDb: Database) {}
 
   /**
    * Arango system information database; acts as a proxy for the current Arango server connection.
    */
-  static get system(): Database {
+   static get system(): Database {
     if (is.undefined(ArangoStore._systemDb)) {
       throw new Error('No arango connection');
     } else {
@@ -50,12 +34,7 @@ export class ArangoStore {
     ArangoStore._systemDb = new Database(connection);
     return ArangoStore.system;
   }
-
-  /*
-   * Load, save, and access working databases
-   */
-
-
+  
   /**
    * Connects to the Arango server (if necessary) and returns a database instance ready to use
    * with Spidergram data.
@@ -64,19 +43,14 @@ export class ArangoStore {
    * @param connection 
    * @returns 
    */
-  static async open(databaseName: string = 'spidergram', connection: Partial<Config> = {}): Promise<Database> {
+  static async open(databaseName: string = 'spidergram', connection: Partial<Config> = {}): Promise<ArangoStore> {
     if (is.undefined(ArangoStore._systemDb)) {
       ArangoStore.connect(connection);
     }
 
-    if (is.undefined(ArangoStore._activeDb) || ArangoStore._activeDb.name != databaseName) {
-      return this.load(databaseName, true, true)
-        .then(database => {
-          return database;
-        })
-    } else {
-      return Promise.resolve(ArangoStore.db);
-    }
+    return new ArangoStore(
+      await ArangoStore.load(databaseName, true)
+    );
   }
 
   /**
@@ -85,68 +59,77 @@ export class ArangoStore {
    * 
    * @param databaseName 
    * @param initialize 
-   * @param setActiveDatabase 
    * @returns 
    */
-  protected static async load(databaseName: string, initialize = true, setActiveDatabase = true): Promise<Database> {
-    if (is.undefined(ArangoStore._systemDb)) {
-      return Promise.reject(new Error('No connection to Arango server'));
+  protected static async load(databaseName: string, initialize = true): Promise<Database> {
+    const system = ArangoStore.system;
+
+    if (is.emptyStringOrWhitespace(databaseName)) {
+      return Promise.reject(new Error(`Invalid databaseName '${databaseName}'`));
+    }
+    return ArangoStore.system.listDatabases()
+      .then(databases => {
+        if (databases.includes(databaseName)) {
+          return system.database(databaseName);
+        } else {
+          return system.createDatabase(databaseName);
+        }
+      })
+      .then(database => {
+        return ArangoStore.initialize(database)
+      });
+  }
+
+  /**
+   * The current active Arango database.
+   */
+  get db(): Database {
+    if (is.undefined(this._activeDb)) {
+      throw new Error('No working database loaded');
     } else {
-      if (is.emptyStringOrWhitespace(databaseName)) {
-        return Promise.reject(new Error(`Invalid databaseName '${databaseName}'`));
-      }
-      return this.system.listDatabases()
-        .then(databases => {
-          if (databases.includes(databaseName)) {
-            return this.system.database(databaseName);
-          } else {
-            return this.system.createDatabase(databaseName);
-          }
-        })
-        .then(database => {
-          if (setActiveDatabase) ArangoStore._activeDb = database;
-          return this.initialize(database)
-        });
+      return this._activeDb!;
     }
   }
 
-  static async initialize(database?: Database, erase = false): Promise<Database> {
+
+  /*
+   * Load, save, and access working databases
+   */
+
+  static async initialize(database: Database, erase = false): Promise<Database> {
     // Ugly shim to ensure all of our entity types are present before initializing.
     const includedTypes = [UniqueUrl, RespondsWith, Resource, LinksTo, IsChildOf, IsVariantOf, AppearsOn];
     assert(includedTypes.length > 0);
 
-    const workingDb = database ?? ArangoStore.db;
-
     const promises: Promise<DocumentCollection>[] = [];
     for (let type of Vertice.types.keys()) {
-      workingDb.collection(type).exists().then(exists => {
+      database.collection(type).exists().then(exists => {
         if (!exists) {
           if (Vertice.types.get(type)?.isEdge) {
-            promises.push(workingDb.createEdgeCollection(type));
+            promises.push(database.createEdgeCollection(type));
           } else {
-            promises.push(workingDb.createCollection(type));
+            promises.push(database.createCollection(type));
           }
         }
         else if (erase) {
-          workingDb.collection(type).truncate();
+          database.collection(type).truncate();
         }
       });
     }
 
-    return Promise.all(promises).then(() => workingDb);
+    return Promise.all(promises).then(() => database);
   }
 
   /*
    * Convenience wrappers for bulk saving and deleting of arbitrary documents
    */
 
-  static add(input: Vertice | Vertice[], database?: Database): Promise<DocumentMetadata[]> {
-    return ArangoStore.set(input, database, false);
+  async add(input: Vertice | Vertice[]): Promise<DocumentMetadata[]> {
+    return this.set(input, false);
   }
 
-  static async set(input: Vertice | Vertice[], database?: Database, overWriteExisting: boolean = true): Promise<DocumentMetadata[]> {
+  async set(input: Vertice | Vertice[], overWriteExisting: boolean = true): Promise<DocumentMetadata[]> {
     const promises: Promise<DocumentMetadata>[] = [];
-    const workingDb = database ?? ArangoStore.db;
     const overwriteMode = (overWriteExisting) ? 'replace' : 'ignore';
     if (!is.array(input)) input = [input];
     
@@ -154,33 +137,32 @@ export class ArangoStore {
     // save all vertices before saving edges.
     for (const vertice of input) {
       if (!isEdge(vertice)) {
-        promises.push(workingDb.collection(vertice._collection)
+        promises.push(this.db.collection(vertice._collection)
           .save(vertice.toJSON(), { overwriteMode: overwriteMode }));
       }
     }
     for (const edge of input) {
       if (isEdge(edge)) {
-        promises.push(workingDb.collection(edge._collection)
+        promises.push(this.db.collection(edge._collection)
           .save(edge.toJSON(), { overwriteMode: overwriteMode }));
       }
     }
     return Promise.all(promises);
   }
 
-  static async delete(input: Vertice | Vertice[], database?: Database) {
+  async delete(input: Vertice | Vertice[]) {
     const promises: Promise<DocumentMetadata>[] = [];
-    const workingDb = database ?? ArangoStore.db;
     if (!is.array(input)) input = [input];
     
     // When bulk deleting, remove edges first.
     for (const edge of input) {
       if (isEdge(edge)) {
-        promises.push(workingDb.collection(edge._collection).remove(edge.id));
+        promises.push(this.db.collection(edge._collection).remove(edge.id));
       }
     }
     for (const vertice of input) {
       if (!isEdge(vertice)) {
-        promises.push(workingDb.collection(vertice._collection).remove(vertice.id));
+        promises.push(this.db.collection(vertice._collection).remove(vertice.id));
       }
     }
     return Promise.all(promises);
