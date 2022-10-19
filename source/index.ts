d8ca429230ee1e9b5d97b647891a6e1dad33d4dd
Jeff Eaton <jeff@eaton.fyi>
2022-10-19 15:27:49 -0500

    Updating exports

--- source/index.ts
+++ source/index.ts
@@ -1,4 +1,7 @@
 export * from './types.js';
 export * from './arango-store.js';
+export * from './spreadsheet.js';
+
 export * from './model/index.js';
 export * from './analysis/index.js';
+export * from './spider/index.js';
