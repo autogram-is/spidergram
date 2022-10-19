085acb0c65d4ba7c9b346b60b785c00a19f67f3f
Jeff Eaton <jeff@eaton.fyi>
2022-10-18 19:01:29 -0500

    Cleaning up DLs; the pattern may work for html too

--- source/spider/handlers/download-handler.ts
+++ source/spider/handlers/download-handler.ts
@@ -1,14 +1,19 @@
 import { BrowserCrawlingContext, InternalHttpCrawlingContext, KeyValueStore } from "crawlee";
 import { SpiderLocalContext } from "../options.js";
+import { fileNameFromHeaders } from "../mime.js";
+import { URL } from 'node:url';
 import * as helpers from '../spider-helper.js';
 
 export async function download<C extends InternalHttpCrawlingContext | BrowserCrawlingContext>(context: SpiderLocalContext & C): Promise<void> {
-  const { sendRequest } = context;
-  context.resource = await helpers.saveResource(context, { files: [] });
+  const { sendRequest, storage } = context;
+  context.resource = await helpers.saveResource(context);
 
   const downloadStore = await KeyValueStore.open('downloads');
-  const buffer = sendRequest({ responseType: 'buffer' });
-  await downloadStore.setValue(`${context.resource.key}.png`, buffer);
+  const buffer = await sendRequest({ responseType: 'buffer' });
+  const fileName = context.resource.key + '-' + fileNameFromHeaders(new URL(buffer.url), buffer.headers);
+  await downloadStore.setValue(fileName, buffer);
 
-  return;
+  context.resource.downloads = [fileName];
+  await storage.push(context.resource);
+  return Promise.resolve();
 }
