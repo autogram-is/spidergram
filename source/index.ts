47116ddff7c43e00e680f8a20aae8d7b848f19ce
Jeff Eaton <jeff@eaton.fyi>
2022-10-24 20:04:33 -0500

    Just slamming in some exports, we'll see how that goes.

--- source/index.ts
+++ source/index.ts
@@ -6,3 +6,6 @@ export * from './model/index.js';
 export * from './analysis/index.js';
 export * from './reports/index.js';
 export * from './spider/index.js';
+
+export * from '@autogram/url-tools';
+export * from 'arangojs';
