f7b197cf134b7d4440a1a0e0ae6da174b7c2277c
Jeff Eaton <jeff@eaton.fyi>
2022-10-27 18:19:01 -0500

    Adding a kludgy visualization wrapper

---
+++ source/reports/visualization.ts
@@ -0,0 +1,84 @@
+import { JSDOM } from 'jsdom';
+import * as d3 from 'd3';
+
+// Shamelessly yoinked from https://github.com/d3-node/d3-node
+// and wrapped in some Typescript-friendly-ifying. We'll be 
+// consolidating this and wrapping it in canvas-based image exports
+// shortly, as well. Usage is fairly straightforward: 
+
+export interface VisuzlizationOptions {
+  html?: string,
+  selector?: string,
+  styles?: string,
+}
+
+export class Visualization {
+  dom: JSDOM;
+  selector: string;
+  document: Document;
+  window: Window & typeof globalThis | null
+  element: d3.Selection<HTMLElement, unknown, d3.BaseType, unknown>;
+
+  constructor (public options: VisuzlizationOptions = {}) {
+    this.dom = new JSDOM(options.html);
+    this.document = this.dom.window.document;
+    this.element = d3.select(this.document.body);
+    if (options.selector) this.element = this.element.select(options.selector);
+  }
+
+  fixXmlCase(text: string) {
+    // Fix a jsdom issue where all SVG tagNames are lowercased:
+    // https://github.com/tmpvar/jsdom/issues/620
+    const tagNames = ['linearGradient', 'radialGradient', 'clipPath', 'textPath'];
+    for (let i = 0, l = tagNames.length; i < l; i++) {
+      const tagName = tagNames[i];
+      text = text.replace(
+        new RegExp('(<|</)' + tagName.toLowerCase() + '\\b', 'g'),
+        (all, start) => { return start + tagName }
+      );
+    }
+    return text;
+  }
+
+  createSVG(width?: number, height?: number, attrs?: Record<string, string | number>) {
+    const svg = this.element.append('svg')
+      .attr('xmlns', 'http://www.w3.org/2000/svg');
+    
+    if (width && height) {
+      svg.attr('width', width).attr('height', height);
+    }
+
+    if (attrs) {
+      Object.keys(attrs).forEach(function (key) {
+        svg.attr(key, attrs[key])
+      });
+    }
+
+    if (this.options.styles) {
+      svg.append('defs')
+        .append('style')
+        .attr('type', 'text/css')
+        .text(this.escapeStyles(this.options.styles))
+    }
+    return svg;
+  }
+
+  svgString() {
+    return this.fixXmlCase(this.element.select<SVGElement>('svg').node()?.outerHTML ?? '');
+  }
+
+  html() {
+    return this.dom.serialize();
+  }
+
+  chartHTML() {
+    if (this.options.selector) {
+      return this.document.querySelector(this.options.selector)?.outerHTML ?? '';
+    }
+    return '';
+  }
+
+  escapeStyles(styles?: string) {
+    return `<![CDATA[ ${styles} ]]>`;
+  }
+}
