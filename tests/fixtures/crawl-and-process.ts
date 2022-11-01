7fa06d11593e31bf5011d060f97fd3ddabb05145
Jeff Eaton <jeff@eaton.fyi>
2022-11-01 12:37:18 -0500

    Putting a pin in a new crawling example

---
+++ tests/fixtures/crawl-and-process.ts
@@ -0,0 +1,45 @@
+import {
+  Resource,
+  Project,
+  VerticeWorker,
+  PlaywrightSpider,
+  aql,
+  UrlHierarchy,
+} from "../../source/index.js";
+
+import {
+  metadata,
+  htmlToText,
+  readabilityScores,
+  cheerio
+} from '../../source/analysis/index.js';
+
+const context = await Project.context({ name: 'ethan' });
+await context.graph.erase({ eraseAll: true });
+
+const spider = new PlaywrightSpider();
+await spider.run(['https://ethanmarcotte.com'])
+  .then(results => console.log);
+
+const worker = new VerticeWorker<Resource>({
+  collection: 'resources',
+  filter: aql`FILTER item.body != null`,
+  task: async (resource, context) => {
+    const $ = cheerio.load(resource.body!)
+
+    resource.meta = metadata($);
+    resource.text = htmlToText(resource.body!, {
+      baseElements: {selectors: ['section.page-content', ]},
+    });
+    resource.readability = readabilityScores(resource.text as string);
+
+    await context.graph.push(resource);
+  }
+});
+
+const urlHier = new UrlHierarchy(context.graph);
+await urlHier.loadPool()
+  .then(async () => urlHier.buildRelationships())
+  .then(async () => urlHier.save());
+
+console.log(await worker.run());
