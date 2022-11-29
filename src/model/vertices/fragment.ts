3798e02b187a171014c8859684f262c90f9e7d0f
Jeff Eaton <jeff@eaton.fyi>
2022-11-29 14:38:06 -0600

    Adding 'fragment' entity for ad-hoc storage of sub-page elements

---
+++ src/model/vertices/fragment.ts
@@ -0,0 +1,43 @@
+import {Vertice, VerticeData, Reference} from '../index.js';
+
+export interface FragmentData extends VerticeData {
+  location?: Reference;
+};
+
+/**
+ * A sub-page content element without a standalone URL of its own.
+ *
+ * @export
+ * @class Fragment
+ * @typedef {Fragment}
+ * @extends {Vertice}
+ */
+export class Fragment extends Vertice {
+  readonly _collection = 'fragments';
+  
+  /**
+   * Optional reference to the graph entity where the fragment was found.
+   * 
+   * NOTE: If an entity ID is passed in as a string it must be a full Arango ID, i.e., 'collection_name/12346...'
+   * 
+   * @param {Vertice | string} input
+   */
+  location?: string;
+
+  /**
+   * Creates an instance of a Fragment, with an optional connection to an existing graph Entity.
+   *
+   * @constructor
+   * @param {FragmentData} input
+   */
+  constructor(input: FragmentData) {
+    const {location, ...dataForSuper} = input;
+    super(dataForSuper);
+
+    if (location) {
+      this.location = Vertice.idFromReference(location);
+    }
+  }
+}
+
+Vertice.types.set('fragments', {constructor: Fragment});
