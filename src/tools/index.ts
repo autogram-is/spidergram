59184fe5022952da744e84c277f56f61df04382b
Jeff Eaton <jeff@eaton.fyi>
2022-12-04 01:33:08 -0600

    Simple tool to grab GA tag manager data

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,6 +1,7 @@
 export * as TextTools from './text.js';
 export * as HtmlTools from './html.js';
 export * as GoogleTools from './google/index.js';
+export * as PageUtils from './page.js';
 
 export * from './screenshot.js';
 export * from './spreadsheet.js';
---
+++ src/tools/page.ts
@@ -0,0 +1,6 @@
+import { Page } from 'playwright';
+
+export async function getGoogleTagData(page: Page) {
+  return page.evaluate<unknown[]>('() => JSON.parse(JSON.stringify(window.dataLayer ?? []))');
+}
+
