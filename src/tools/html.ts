87ebf78e3b566550a433a0d50a152b44145f837d
Jeff Eaton <jeff@eaton.fyi>
2022-12-04 00:16:07 -0600

    Removing getSchemaOrg function

--- src/tools/html.ts
+++ src/tools/html.ts
@@ -8,7 +8,6 @@ import { htmlToText, HtmlToTextOptions } from 'html-to-text';
 import { Properties } from '../model/helpers/properties.js';
 
 export { social as getSocialLinks, parseOpenGraph as getOpenGraph } from 'crawlee';
-import { Thing, Graph as SchemaGraph } from "schema-dts";
 import { Resource } from '../model/index.js';
 
 export function getPlainText(html: string, options?: HtmlToTextOptions): string {
@@ -64,42 +63,6 @@ export function getMetadata(input: string | Resource, baseUrl?: string) {
     }
     return output;
   }
-}
-
-// We'll want to deprecate this; Properties in particular is jank; a relic of some 
-// bad JSON-parsing practices from early in the project.
-export function getMetaTags(input: string | cheerio.Root): Properties<string> {
-  const $ = is.string(input) ? parseWithCheerio(input) : input;
-  const results: Record<string, string> = {};
-
-  $('head meta').each((index, tag) => {
-    const k = $(tag).attr('name') || $(tag).attr('property') || $(tag).attr('itemprop')
-    const v = $(tag).attr('content')  
-    if (k && v) results[k] = v;
-  });
-
-  const title = $('head title').text().toString();
-  if (title) results.title = title;
-
-  return results;
-}
-
-// Right now this only works with a single @graph element. Later we'll want to graph
-// any JSON-LD chunks and turn them into an array.
-export function getSchemaOrg(input: string | cheerio.Root): readonly Thing[] {
-  const $ = is.string(input) ? parseWithCheerio(input) : input;
-  let results: readonly Thing[] = [];
-  try {
-    $('script[type=application/ld+json]').each((index, element) => {
-      const json = JSON.parse($(element).text()) as SchemaGraph;
-      if ('@context' in json && json["@context"] == 'https://schema.org') {
-        results = json["@graph"];
-      }  
-    })
-  } catch {
-    results = [];
-  }
-  return results;
 }
 
 export function getBodyAttributes(input: string | cheerio.Root): Properties<string> {
