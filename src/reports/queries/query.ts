f85169ebaab54511fe7bef5a68445d35eeb376f1
Jeff Eaton <jeff@eaton.fyi>
2022-12-14 16:22:43 -0600

    Adding a simple Query helper class; can be subclassed to handle paging, or paramaterized generation of dynamic queries.

---
+++ src/reports/queries/query.ts
@@ -0,0 +1,29 @@
+import { Project } from '../../index.js'
+import { QueryOptions, Database } from 'arangojs/database.js';
+import { GeneratedAqlQuery } from 'arangojs/aql.js'
+import { AnyJson } from '@salesforce/ts-types';
+
+export { QueryOptions } from 'arangojs/database.js';
+
+export class Query {
+  constructor(protected _query: GeneratedAqlQuery) { }
+  
+  static db?: Database;
+
+  static async run<T = AnyJson>(query: GeneratedAqlQuery, options: QueryOptions = {}) {
+    if (this.db === undefined) {
+      this.db = await Project.config()
+        .then(project => project.graph())
+        .then(graph => graph.db);
+    }
+    return this.db!.query<T>(query, options).then(cursor => cursor.all());
+  }
+
+  get query() {
+    return this._query;
+  }
+
+  async run<T = AnyJson>(options: QueryOptions = {}) {
+    return Query.run(this.query, options);
+  }
+}
