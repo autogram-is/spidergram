2d74975d9d5f4947b180e148f841c0348fa5185c
Jeff Eaton <jeff@eaton.fyi>
2023-01-04 17:12:40 -0600

    Ahem, including findPatterns as well

--- src/tools/html/index.ts
+++ src/tools/html/index.ts
@@ -3,5 +3,6 @@ export * from './get-body-attributes.js';
 export * from './get-metadata.js';
 export * from './get-plaintext.js';
 export * from './parse-with-cheerio.js';
+export * from './find-patterns.js';
 
 export { social as getSocialLinks } from 'crawlee';
