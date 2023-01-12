f3f89620563f769fa7fe7b7a5b8f81e39c548220
Jeff Eaton <jeff@eaton.fyi>
2023-01-12 17:02:43 -0600

    Improve tsdoc for meta parsing, use generic element attribute parsing for body attributes.

--- src/tools/html/get-page-data.ts
+++ src/tools/html/get-page-data.ts
@@ -5,15 +5,62 @@ import { parseElementsToArray, parseElementsToDictionary } from './parse-element
 import { parseMetatags, MetaValues } from "./parse-meta.js";
 import { getElementAttributes, ElementAttributes } from "./get-element-attributes.js";
 
+
+/**
+ * Options to control extraction of structured data from HTML pages
+ */
 type PageDataOptions = {
+  /**
+   * Parse all available information, ignoring any other 'false' parameters set in
+   * the options object.
+   */
+  all?: boolean,
+  /**
+   * Parse and list the attributes of the HTML `<body>` tag. Body classes and IDs are
+   * often populated with contextual and content related metadata by CMSs templates.
+   */
   attributes?: boolean,
+  /**
+   * Extract common HTML head sub-tags like `<title>`, `<base>`, and so on.
+   */
   head?:  boolean,
+  /**
+   * Parse and list any `<meta>` tags present in the document. These will be returned
+   * as a dictionary keyed by the 'name', 'itemprop', and 'property' attributes of the
+   * meta tags; keys with colons will be treated as nested; `<meta name="og:title" ...>`
+   * for example will become become `meta['og']['title'] = ...`
+   */
   meta?: boolean,
+  /**
+   * Parse and list any `<link>` tags present in the document. These will be returned
+   * as a dictionary keyed by the links' `rel` attributes.
+   */
   links?: boolean,
+  /**
+   * Parse and list any `<noscript>` tags present in the document.
+   */
   noscript?: boolean,
+  /**
+   * Parse and list any `<script>` tags in the document; JSON data will be parsed
+   * and stored in a separate 'json' property of the results
+   */
   scripts?: boolean,
+  /**
+   * Parse and list any CSS `<style>` tags present in the document.
+   */
   styles?: boolean,
+  /**
+   * Parse and list any HTML `<template>` tags present in the document.
+   */
   templates?: boolean,
+  /**
+   * Ignore rather than modifying tags and structured data that are present but
+   * incomplete or in the wrong location. Turning on 'strict' mode will throw away
+   * quite a bit of data, because everyone on the planet outputs horribly malformed
+   * meta tags, RDFa gunk, and so on. It's a plague. Nightmarish stuff, really.
+   */
+  strict?: boolean,
+  metaArrayAttributes?: string[],
 }
 
 export const defaultOptions = {
@@ -53,14 +100,14 @@ export function getPageData(input: string | cheerio.Root, customOptions: PageDat
   const results: PageData = { };
   const options = _.defaultsDeep(customOptions, defaultOptions);
   
-  if (options.attributes) {
-    const attributes = getElementAttributes($, $('body')[0]);
+  if (options.attributes || options.all) {
+    const attributes = getElementAttributes($('body'));
     if (Object.entries(attributes).length) {
       results.attributes = attributes;
     }
   }
 
-  if (options.head) {
+  if (options.head || options.all) {
     const title = $('title').first().html()?.toString().trim() ?? undefined;
     if (title) {
       results.title = title;
@@ -77,42 +124,42 @@ export function getPageData(input: string | cheerio.Root, customOptions: PageDat
     }
   }
 
-  if (options.meta) {
+  if (options.meta || options.all) {
     const headMeta = $('meta').toArray().map(element => $(element).attr());
     if (Object.entries(headMeta).length) {
       results.meta = parseMetatags(headMeta);
     }
   }
 
-  if (options.links) {
+  if (options.links || options.all) {
     const links = parseElementsToDictionary($, 'link', 'rel');
     if (links) {
       results.links = links;
     }
   }
 
-  if (options.noscript) {
+  if (options.noscript || options.all) {
     const noscript = parseElementsToArray($, 'noscript');
     if (noscript.length) {
       results.noscript = noscript;
     }
   }
 
-  if (options.templates) {
+  if (options.templates || options.all) {
     const templates = parseElementsToArray($, 'template');
     if (templates.length) {
       results.templates = templates;
     }
   }
 
-  if (options.styles) {
+  if (options.styles || options.all) {
     const styles = parseElementsToArray($, 'style');
     if (styles.length) {
       results.styles = styles;
     }
   }
 
-  if (options.scripts) {
+  if (options.scripts || options.all) {
     const json: Record<string, string | unknown>[] = [];
     const scripts: Record<string, string | undefined>[] = [];
 
