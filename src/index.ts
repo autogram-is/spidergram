57ed46a8812a27d12053b0a937b550d42f69402a
Eaton <jeff@eaton.fyi>
2023-01-12 21:25:08 -0600

    Fix broken Report queries (#47)
    * Export TS-Types utilities for consumer projects
    * Update Spreadsheet class for better type-safety. Now with basic unit tests.
    * Adding @ava/cooperate to enable smoother tests

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 export * from '@autogram/url-tools';
-export * as crawlee from 'crawlee';
-export * as arangojs from 'arangojs';
+export * as Typing from '@salesforce/ts-types';
+export * as Crawlee from 'crawlee';
+export * as Arango from 'arangojs';
+
 import lodash from 'lodash';
 export const _ = lodash;
 
 export * from './cli/index.js';
-
 export * from './model/index.js';
 export * from './services/index.js';
 export * from './spider/index.js';
---
+++ tests/spreadsheet.test.ts
@@ -0,0 +1,52 @@
+import test from 'ava';
+import { Spreadsheet, Query, aql } from "../src/index.js";
+import * as fs from 'fs/promises';
+
+test('query results treated as sheet', async t => {
+  const data = await Query.run(aql`
+    FOR r IN resources
+    LIMIT 10
+    RETURN {
+      url: r.url,
+      status: r.code,
+      mime: r.mime
+    }
+  `);
+
+  const report = new Spreadsheet();
+  report.addSheet(data, 'report');
+  
+  await t.notThrowsAsync(
+    report.save('./tests/spreadsheet-simple')
+      .then(fileName => fs.stat(fileName)
+      .then(stats => stats ? fileName : false ))
+      .then(fileName => fileName ? fs.rm(fileName) : {} )
+  );
+})
+
+test('sheet structure respected', async t => {
+  const data = await Query.run(aql`
+    FOR r IN resources
+    LIMIT 10
+    RETURN {
+      url: r.url,
+      status: r.code,
+      mime: r.mime
+    }
+  `);
+
+  const report = new Spreadsheet();
+  report.addSheet({
+    name: 'Sheet 1',
+    data,
+    header: ['URL', 'HTTP Status', 'MIME Type']
+  });
+  
+  await t.notThrowsAsync(
+    report.save('./tests/spreadsheet-complex')
+      .then(fileName => fs.stat(fileName)
+      .then(stats => stats ? fileName : false ))
+      .then(fileName => fileName ? fs.rm(fileName) : {} )
+  );
+})
+
