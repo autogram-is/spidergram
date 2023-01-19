70ebbb52743d827a87d2f5c74fee5a22fd8cfd88
Jeff Eaton <jeff@eaton.fyi>
2023-01-19 15:16:27 -0600

    Simple summary-report query generator. It's a monstrous abomination of AQL literals, but for the time being it works.

--- src/reports/queries/index.ts
+++ src/reports/queries/index.ts
@@ -2,3 +2,4 @@ export * from './query.js';
 export * from './vertice-query.js';
 export * from './uncrawled-url-query.js';
 export * from './link-summaries.js';
+export * from './property-summary.js';
---
+++ tests/property-summary-query.test.ts
@@ -0,0 +1,13 @@
+import test from 'ava';
+import { Query, getPropertySummary } from "../src/index.js";
+
+test('grouped query runs', async t => {
+  const query = getPropertySummary('resources', {
+    properties: ['data.head.title'],
+    groupBy: ['parsed.domain'],
+    includeTotal: true
+  });
+  const results = await Query.run(query);
+  console.log(results);
+  t.assert(results.length > 0);
+});
