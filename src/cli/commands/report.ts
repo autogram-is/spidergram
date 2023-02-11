116aa2a71a0f8a064dc8978d6f104c4ff9ee252e
Jeff Eaton <jeff@eaton.fyi>
2023-02-11 12:50:16 -0600

    Renaming 'query' to 'report'

--- src/cli/commands/report.ts
+++ src/cli/commands/report.ts
@@ -1,111 +1,157 @@
 import { OutputLevel } from '../../index.js';
 import { SgCommand } from '../index.js';
 import { Flags } from '@oclif/core';
-import { Query as QueryBuilder } from '../../reports/index.js'
+import { AqFilter, Query } from '../../reports/index.js'
 import { Spreadsheet } from '../../index.js';
-import { JsonMap } from '@salesforce/ts-types';
+import { JsonMap, JsonPrimitive } from '@salesforce/ts-types';
 import _ from 'lodash';
 import { readFile } from 'fs/promises';
 import { aql, literal, GeneratedAqlQuery } from 'arangojs/aql.js'
 import * as csv from 'fast-csv';
-import is from '@sindresorhus/is';
+import arrify from 'arrify';
 
-export default class Query extends SgCommand {
-  static aliases = ['report'];
+export default class Report extends SgCommand {
+  static strict = false;
 
   static summary = 'Query the Spidergram crawl data';
+
+  static usage = '<%= command.id %> [--input <value> | --aql <value> | --collection=<value>] ...';
 
   static flags = {
     // Basic query information
     input: Flags.string({
       char: 'i',
-      summary: 'A file containing an Arango query spec',
+      summary: 'A JSON file containing a query description',
+      exclusive: ['aql']
     }),
     output: Flags.string({
       char: 'o',
-      summary: 'Output filename',
+      summary: 'Output format or filename',
+      description:
+`table: (Default) Displays a formatted table of results
+debug: Display the query spec and generated AQL statement without running it
+*.csv: Save the results as a CSV file in the storage directory
+*.json: Save the results as a JSON file in the storage directory.
+*.xlsx: Save the results as an Excel workbook in the storage directory`,
     }),
+    aql: Flags.string({
+      char: 'a',
+      summary: 'A file containing a raw AQL query',
+    }),
+
     collection: Flags.string({
       char: 'c',
+      exclusive: ['aql'],
       summary: 'The Arango collection to be queried',
-      default: 'resources'
+      default: 'resources',
+      helpGroup: 'Query'
     }),
     filter: Flags.string({
       char: 'f',
-      summary: '-',
+      exclusive: ['aql'],
+      summary: 'Filter records by a property',
+      description: 
+`[path] returns records where the property IS NOT null
+[path = value] returns records where property equals value
+[path != value] returns records where property DOES NOT equal value
+[path > value] returns records where property is greater than value
+[path < value] returns records where property is less than value
+[path { value,value] returns records where values contain property
+[path } value] returns records where value is contained in property`,
       multiple: true,
+      helpGroup: 'Query'
     }),
     return: Flags.string({
       char: 'r',
+      exclusive: ['aql'],
       summary: 'Properties to include in the return value',
-      multiple: true
-    }),
-    returnFilter: Flags.string({
-      aliases: ['rf'],
-      summary: '-',
       multiple: true,
+      helpGroup: 'Query'
+    }),
+    postFilter: Flags.string({
+      char: 'p',
+      exclusive: ['aql'],
+      summary: 'Filter post-aggregation results by a property',
+      multiple: true,
+      helpGroup: 'Query'
     }),
     sort: Flags.string({
+      char: 's',
+      exclusive: ['aql'],
       summary: 'Sort results by a property',
       multiple: true,
+      helpGroup: 'Query'
     }),
     limit: Flags.integer({
+      char: 'l',
+      exclusive: ['aql'],
       summary: 'The Arango collection to be queried',
-      default: 20
+      default: 20,
+      helpGroup: 'Query'
     }),
 
     // Collect/aggregate
     group: Flags.string({
+      char: 'g',
       aliases: ['collect'],
+      exclusive: ['aql'],
       summary: 'Group the results by a property',
       multiple: true,
       helpGroup: 'Aggregation'
     }),
     empty: Flags.string({
+      exclusive: ['aql'],
       summary: 'COUNT_EMPTY() a property when grouping results',
       multiple: true,
       helpGroup: 'Aggregation'
     }),
     nonempty: Flags.string({
+      exclusive: ['aql'],
       summary: 'COUNT_EMPTY() a property when grouping results',
       multiple: true,
       helpGroup: 'Aggregation'
     }),
     distinct: Flags.string({
+      exclusive: ['aql'],
       summary: 'COUNT_DISTINCT() a property when grouping results',
       multiple: true,
       helpGroup: 'Aggregation'
     }),
     sum: Flags.string({
+      exclusive: ['aql'],
       summary: 'SUM() a property when grouping results',
       multiple: true,
       helpGroup: 'Aggregation'
     }),
     min: Flags.string({
+      exclusive: ['aql'],
       summary: 'MIN() a property when grouping results',
       multiple: true,
       helpGroup: 'Aggregation'
     }),
     max: Flags.string({
+      exclusive: ['aql'],
       summary: 'MAX() a property when grouping results',
       multiple: true,
       helpGroup: 'Aggregation'
     }),
     avg: Flags.string({
+      exclusive: ['aql'],
       summary: 'AVG() a property when grouping results',
       multiple: true,
       helpGroup: 'Aggregation'
     }),
-    total: Flags.string({
+    count: Flags.string({
+      exclusive: ['aql'],
+      char: 'c',
       summary: 'Label for for grouping subtotals',
+      default: 'total',
       helpGroup: 'Aggregation'
     }),
   };
 
-  static strict = false;
-
   async run() {
-    const { flags } = await this.parse(Query);
+    const { flags } = await this.parse(Report);
     const { project } = await this.getProjectContext(false);
 
     if (flags.output === 'json') {
@@ -114,26 +160,27 @@ export default class Query extends SgCommand {
       this.output = OutputLevel.interactive;
     }
 
-    let qb: QueryBuilder | undefined;
+    // Set up the placeholder values and generated queries
+    let qb: Query | undefined;
     let q: GeneratedAqlQuery | undefined;
-
-    if (flags.input) {
-      if (flags.input.endsWith('.aql')) {
-        const aqlString = await readFile(flags.input).then(buffer => buffer.toString());
-        q = aql`${literal(aqlString)}`;
-      } else {
-        const spec = await readFile(flags.input).then(buffer => JSON.parse(buffer.toString()));
-        qb = new QueryBuilder(spec);
-      }
-    }
-
-    // If there's no GeneratedAqlQuery yet, try building it from the QueryBuilder,
-    // if it exists. Very speculative stuff. Also set up an empty result set.
-    if (q === undefined) {
-      qb = await this.buildQueryFromFlags(qb);
-      q = qb?.build();
-    }
     let results: JsonMap[] = [];
+
+    if (flags.aql) {
+      // If we received a raw AQL file, we'll skip the majority of our builder code and
+      // go straight to display.
+      const aqlString = await readFile(flags.aql).then(buffer => buffer.toString());
+      q = aql`${literal(aqlString)}`;
+    }
+    else if (flags.input) {
+      // If a custom spec was passed in, load it and then process the rest of the flags.
+      const spec = await readFile(flags.input).then(buffer => JSON.parse(buffer.toString()));
+      qb = await this.buildQueryFromFlags(new Query(spec));
+      q = qb.build();
+    } else {
+      // Aaaand finally, if no other inputs were specified just do the usual processing.
+      qb = await this.buildQueryFromFlags();
+      q = qb.build();
+    }
 
     // Now we start handling output; 'debug' and 'spec' mode are meant to help
     // When a query errors out, so we'll handle them without actually executing it.
@@ -149,7 +196,7 @@ export default class Query extends SgCommand {
         this.ux.error(`Couldn't build query; try using --output=debug to inspect the generated query.`);
       } else {
         this.ux.action.start('Running query');
-        results = await QueryBuilder.run<JsonMap>(q);
+        results = await Query.run<JsonMap>(q);
         this.ux.action.stop();
       }
   
@@ -217,41 +264,23 @@ export default class Query extends SgCommand {
   }
 
   /**
-   * Given the assorted flags that have been passed in, uses the QueryBuilder's
+   * Given the assorted flags that have been passed in, uses the Query's
    * fluent methods to build out a full query spec.
-   * 
-   * NOTE: Due to the way flags are grouped by OCLIF, there's no way to put
-   * one filter *before* the group-by commands. Thus, all filters must be
-   * applied before grouping/aggregation. This makes it impossible to build, for
-   * example, a query that counts the number of items in a category, but hides
-   * categoies with fewer than 10 items.
-   * 
-   * We're working on it.
    */
-  async buildQueryFromFlags(qb?: QueryBuilder) {
-    const { flags } = await this.parse(Query);
+  async buildQueryFromFlags(qb?: Query) {
+    const { flags } = await this.parse(Report);
 
-    if (qb === undefined) qb = new QueryBuilder(flags.collection);
+    if (qb === undefined) qb = new Query(flags.collection);
 
     // Filters
     for (const f of flags.filter ?? []) {
-      if (f.indexOf(':') >= 0) {
-        const [prop, value] = f.split(':');
-        const multiValues: (string | number)[] = value.split(',').map(item => item.trim()).map(item => is.numericString(item) ? Number.parseInt(item) : item)
-        if (multiValues.length > 1) {
-          qb.filterBy(prop, multiValues);
-        } else {
-          qb.filterBy(prop, is.numericString(value) ? Number.parseInt(value) : value);
-        }
-      } else {
-        qb.filterBy(f);
-      }
+      qb.filterBy(buildFilter(f));
     }
 
     // Group/Collect
     for (const r of flags.group ?? []) {
-      if (r.indexOf(':') >= 0) {
-        qb.groupBy(r.split(':')[0], r.split(':')[1]);
+      if (r.indexOf('=') >= 0) {
+        qb.groupBy(r.split('=')[0], r.split('=')[1]);
       } else {
         qb.groupBy(r);
       }
@@ -262,7 +291,7 @@ export default class Query extends SgCommand {
     const numericFuncs = ['sum', 'avg', 'min', 'max'];
     for (const fnc of aggregateFuncs) {
       for (const a of flags[fnc] ?? []) {
-        const [name, path] = a.split(':');
+        const [name, path] = a.split('=');
         if (numericFuncs.includes(a)) {
           qb.aggregate({ name, aggregate: fnc}, fnc, path);
         } else {
@@ -273,31 +302,22 @@ export default class Query extends SgCommand {
 
     // Return properties
     for (const r of flags.return ?? []) {
-      if (r.indexOf(':') >= 0) {
-        qb.return(r.split(':')[0], r.split(':')[1]);
+      if (r.indexOf('=') >= 0) {
+        const [name, path] = r.split('=');
+        qb.return(name, path);
       } else {
         qb.return(r);
       }
     }
   
     // Post-collect filters
-    for (const f of flags.returnFilter ?? []) {
-      if (f.indexOf(':') >= 0) {
-        const [prop, value] = f.split(':');
-        const multiValues: (string | number)[] = value.split(',').map(item => item.trim()).map(item => is.numericString(item) ? Number.parseInt(item) : item)
-        if (multiValues.length > 1) {
-          qb.filterBy(prop, multiValues);
-        } else {
-          qb.filterBy(prop, is.numericString(value) ? Number.parseInt(value) : value);
-        }
-      } else {
-        qb.filterBy(f);
-      }
+    for (const f of flags.postFilter ?? []) {
+      qb.filterBy(buildFilter(f));
     }
 
     // Sort
     for (const s of flags.sort ?? []) {
-      const [prop, direction] = s.split(':');
+      const [prop, direction] = s.split('=');
       if (direction === 'asc' || direction === 'desc') {
         qb.sortBy(prop, direction);
       } else {
@@ -305,17 +325,23 @@ export default class Query extends SgCommand {
       }
     }
 
+    if (flags.count?.toLocaleLowerCase() === 'false') {
+      qb.count(false);
+    } else {
+      qb.count(flags.count);
+    }
+
     qb.limit(flags.limit);
 
     return Promise.resolve(qb);
   }
 
-  async printSpec(qb?: QueryBuilder) {
+  async printSpec(qb?: Query) {
     this.ux.styledJSON(qb?.spec);
     return Promise.resolve();
   }
 
-  async printDebugData(qb?: QueryBuilder, q?: GeneratedAqlQuery) {
+  async printDebugData(qb?: Query, q?: GeneratedAqlQuery) {
     if (qb) {
       this.ux.info('');
       this.ux.styledHeader('Query spec');
@@ -335,4 +361,67 @@ export default class Query extends SgCommand {
     }
     return Promise.resolve();
   }
+}
+
+function buildFilter(input: string): AqFilter {
+  let filterSpec: AqFilter;
+
+  if (input.indexOf('!=') >= 0) {
+    // property must not equal
+    const [prop, value] = input.split('!=');
+    filterSpec = { path: prop, negate: true };
+    filterSpec['eq'] = coerceValue(value);
+
+  } else if (input.indexOf('=') >= 0 || input.indexOf('==') >= 0) {
+    // property must equal
+    const [prop, value] = input.split(/=+/);
+    filterSpec = { path: prop };
+    filterSpec['eq'] = coerceValue(value);
+
+  } else if (input.indexOf('>') >= 0) {
+    // property must be greater than value
+    const [prop, value] = input.split('>');
+    filterSpec = { path: prop };
+    filterSpec['gt'] = coerceValue(value) ?? undefined;
+
+  } else if (input.indexOf('<') >= 0) {
+    // property must be less than value
+    const [prop, value] = input.split('<');
+    filterSpec = { path: prop };
+    filterSpec['lt'] = coerceValue(value) ?? undefined;
+
+  } else if (input.indexOf('{') >= 0) {
+    // property must be contained in a list of values
+    const [prop, value] = input.split('{');
+    filterSpec = { path: prop };
+    filterSpec['in'] = arrify(splitMultiValues(value));
+
+  } else if (input.indexOf('}') >= 0) {
+    // value must be contained in property, which is a list
+    const [prop, value] = input.split('}');
+    filterSpec = { path: prop };
+    filterSpec['contains'] = coerceValue(value);
+
+  } else {
+    // No equality/comparison operator or explicit value. We'll treat
+    // it as a 'not null'
+    filterSpec = { path: input, eq: null, negate: true };
+  }
+
+  return filterSpec;
+}
+
+function splitMultiValues(value: string) {
+  const multiValues: JsonPrimitive[] = value.split(',').map(item => item.trim()).map(item => coerceValue(item))
+  if (multiValues.length === 0) return multiValues[0];
+  else return multiValues;
+}
+
+function coerceValue(value: string) {
+  const numValue = Number.parseInt(value);
+  if (!Number.isNaN(numValue)) return numValue;
+  else if (value.toLocaleLowerCase() === 'null') return null;
+  else if (value.toLocaleLowerCase() === 'true') return 1;
+  else if (value.toLocaleLowerCase() === 'false') return 0;
+  else return value;
 }
