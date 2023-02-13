fa5d8bfa9f31fa7627cf56b308016831975704e1
Jeff Eaton <jeff@eaton.fyi>
2023-02-13 12:02:38 -0600

    Definitely some jank to it, but wrapping return values in functions is now supported.

--- src/cli/commands/report.ts
+++ src/cli/commands/report.ts
@@ -304,9 +304,9 @@ debug: Display the query spec and generated AQL statement without running it
     for (const r of flags.return ?? []) {
       if (r.indexOf('=') >= 0) {
         const [name, path] = r.split('=');
-        qb.return(name, path);
+        qb.return({ name, ...unwrapPathFunction(path)});
       } else {
-        qb.return(r);
+        qb.return({ ...unwrapPathFunction(r) });
       }
     }
   
@@ -369,43 +369,48 @@ function buildFilter(input: string): AqFilter {
   if (input.indexOf('!=') >= 0) {
     // property must not equal
     const [prop, value] = input.split('!=');
-    filterSpec = { path: prop, negate: true };
-    filterSpec['eq'] = coerceValue(value);
+    const cv = coerceValue(value);
+    filterSpec = { ...unwrapPathFunction(prop), negate: true, eq: cv };
+    if (typeof cv === 'string') filterSpec.type = 'string';
 
   } else if (input.indexOf('=') >= 0 || input.indexOf('==') >= 0) {
     // property must equal
     const [prop, value] = input.split(/=+/);
-    filterSpec = { path: prop };
-    filterSpec['eq'] = coerceValue(value);
+    const cv = coerceValue(value);
+    filterSpec = { ...unwrapPathFunction(prop), eq: cv };
+    if (typeof cv === 'string') filterSpec.type = 'string';
 
   } else if (input.indexOf('>') >= 0) {
     // property must be greater than value
     const [prop, value] = input.split('>');
-    filterSpec = { path: prop };
-    filterSpec['gt'] = coerceValue(value) ?? undefined;
+    const cv = coerceValue(value) ?? undefined;
+    filterSpec = { ...unwrapPathFunction(prop), gt: cv };
+    if (typeof cv === 'string') filterSpec.type = 'string';
 
   } else if (input.indexOf('<') >= 0) {
     // property must be less than value
     const [prop, value] = input.split('<');
-    filterSpec = { path: prop };
-    filterSpec['lt'] = coerceValue(value) ?? undefined;
+    const cv = coerceValue(value) ?? undefined;
+    filterSpec = { ...unwrapPathFunction(prop), lt: cv };
+    if (typeof cv === 'string') filterSpec.type = 'string';
 
   } else if (input.indexOf('{') >= 0) {
     // property must be contained in a list of values
     const [prop, value] = input.split('{');
-    filterSpec = { path: prop };
-    filterSpec['in'] = arrify(splitMultiValues(value));
+    filterSpec = { ...unwrapPathFunction(prop), in: arrify(splitMultiValues(value)) };
+    if (typeof (filterSpec.in ?? [])[0] === 'string') filterSpec.type = 'string';
 
   } else if (input.indexOf('}') >= 0) {
     // value must be contained in property, which is a list
     const [prop, value] = input.split('}');
-    filterSpec = { path: prop };
-    filterSpec['contains'] = coerceValue(value);
+    const cv = coerceValue(value);
+    filterSpec = { ...unwrapPathFunction(prop), contains: cv };
+    if (typeof cv === 'string') filterSpec.type = 'string';
 
   } else {
     // No equality/comparison operator or explicit value. We'll treat
     // it as a 'not null'
-    filterSpec = { path: input, eq: null, negate: true };
+    filterSpec = { ...unwrapPathFunction(input), eq: null, negate: true };
   }
 
   return filterSpec;
@@ -421,7 +426,14 @@ function coerceValue(value: string) {
   const numValue = Number.parseInt(value);
   if (!Number.isNaN(numValue)) return numValue;
   else if (value.toLocaleLowerCase() === 'null') return null;
-  else if (value.toLocaleLowerCase() === 'true') return 1;
-  else if (value.toLocaleLowerCase() === 'false') return 0;
   else return value;
+}
+
+function unwrapPathFunction(input: string) {
+  const [full, func, path] = input.match(/([a-zA-Z0-0_]+)\((.*)\)/) ?? [];
+  if (full) {
+    return { path, function: func };
+  } else {
+    return { path: input };
+  }
 }
