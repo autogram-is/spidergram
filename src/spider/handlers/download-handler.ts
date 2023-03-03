e709572eaa034fe13b6037e2d9779f2ae3857c24
Jeff Eaton <jeff@eaton.fyi>
2023-03-03 16:26:00 -0600

    First pass at a fixed download handler

--- src/spider/handlers/download-handler.ts
+++ src/spider/handlers/download-handler.ts
@@ -1,6 +1,8 @@
 import { Readable } from 'node:stream';
 import { fileNameFromHeaders } from '../helpers/mime.js';
 import { SpiderContext } from '../context.js';
+import { Project } from '../../index.js';
+import { ensureDir } from "fs-extra";
 
 export async function downloadHandler(context: SpiderContext): Promise<void> {
   const { graph, files, saveResource, sendRequest } = context;
@@ -18,7 +20,11 @@ export async function downloadHandler(context: SpiderContext): Promise<void> {
     resource.key +
     '-' +
     fileNameFromHeaders(new URL(buffer.url), buffer.headers);
-  await files('downloads').writeStream(fileName, Readable.from(buffer.rawBody));
+
+  const directory = [resource.parsed.hostname.replaceAll('.', '-'), resource.mime?.replaceAll('/', '-') ?? 'unknown'].join('/')
+  const proj = await Project.config();
+  await ensureDir([proj.root ?? '.', 'storage', 'downloads', directory].join('/'));
+  await files('downloads').writeStream([directory, fileName].join('/'), Readable.from(buffer.rawBody));
 
   resource.payload = { bucket: 'downloads', path: fileName };
   await graph.push(resource);
