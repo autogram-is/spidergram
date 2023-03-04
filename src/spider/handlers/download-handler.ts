6670b77e2be285095ace30e0bb1e496bbba8dd71
Jeff Eaton <jeff@eaton.fyi>
2023-03-03 23:31:40 -0600

    Sketchy but workable; we'll come back to this another time to make sure downloads work well when using proies.

--- src/spider/handlers/download-handler.ts
+++ src/spider/handlers/download-handler.ts
@@ -1,31 +1,34 @@
-import { Readable } from 'node:stream';
+import { Duplex } from 'node:stream';
 import { fileNameFromHeaders } from '../helpers/mime.js';
 import { SpiderContext } from '../context.js';
 import { Project } from '../../index.js';
 import { ensureDir } from "fs-extra";
+import path from 'node:path';
 
 export async function downloadHandler(context: SpiderContext): Promise<void> {
-  const { graph, files, saveResource, sendRequest } = context;
+  const { graph, files, saveResource } = context;
   const resource = await saveResource();
 
-  const buffer = await sendRequest({
-    responseType: 'buffer',
-    resolveBodyOnly: true,
-    allowGetBody: true,
-    decompress: true,
-    method: 'GET',
+  const response = await fetch(resource.parsed)
+  .then(r => {
+    if (r.status !== 200) throw new Error('Could not download');
+    return r;
   });
 
-  const fileName =
-    resource.key +
-    '-' +
-    fileNameFromHeaders(new URL(buffer.url), buffer.headers);
+  if (response.body) {
+    const fileName =
+      resource.key +
+      '-' +
+      fileNameFromHeaders(new URL(resource.url), resource.headers);
 
-  const directory = [resource.parsed.hostname.replaceAll('.', '-'), resource.mime?.replaceAll('/', '-') ?? 'unknown'].join('/')
-  const proj = await Project.config();
-  await ensureDir([proj.root ?? '.', 'storage', 'downloads', directory].join('/'));
-  await files('downloads').writeStream([directory, fileName].join('/'), Readable.from(buffer.rawBody));
+    const directory = path.join(resource.parsed.hostname.replaceAll('.', '-'), resource.mime?.replaceAll('/', '-') ?? 'unknown');
+    const proj = await Project.config();
+    await ensureDir(path.join(proj.root ?? '.', 'storage', 'downloads', directory));
+    const fullPath = path.join(directory, fileName);
+    await files('downloads').writeStream(fullPath, Duplex.from(response.body));
 
-  resource.payload = { bucket: 'downloads', path: fileName };
-  await graph.push(resource);
+    resource.payload = { bucket: 'downloads', path: fullPath };
+    await graph.push(resource);
+  }
+  return Promise.resolve();
 }
