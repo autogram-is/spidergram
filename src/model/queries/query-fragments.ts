54af4ee62c9243d8a0bb93005b30c94ee68ec345
Jeff Eaton <jeff@eaton.fyi>
2023-03-27 13:43:22 -0500

    Adding a handful of additional query fragments

--- src/model/queries/query-fragments.ts
+++ src/model/queries/query-fragments.ts
@@ -1,5 +1,12 @@
 import { AqQuery } from "aql-builder";
 
+/**
+ * Returns a collection of UniqueUrls that *do* have accompanying resource
+ * entries in the crawl database. This can be a useful starting point for
+ * grabbing specific sets of unvisited URLs for follow-up crawls.
+ * 
+ * @example
+ */
 export const uncrawledUrls: AqQuery = {
   "document": "uu",
   "collection": "unique_urls",
@@ -15,11 +22,39 @@ export const uncrawledUrls: AqQuery = {
     },
   ],
   "filters": [
-    { "name": "parsed.protocol", "in": [ "https:", "http:" ] },
     { "document": false, "name": "responses", "eq": 0 },
   ],
-}
+};
 
+/**
+ * Connects the UniqueURLs, ResondsWith, and Resources collections to count the
+ * number of redirects that were encountered when retrieving a page. Only pages
+ * that *were* redirected are returned by this query.
+ * 
+ * The query has three documents:
+ * - uu: UniqueUrls, the default document.
+ * - rw: RespondsWith, the relationship between a UniqueUrl and a Resource. It contains request headers and redirect information.
+ * - rs: Resource, information about the final destination page.
+ * 
+ * @example Retrieve requests for example.com URLs that were redirected
+ * ```
+ * const q = new Query(redirects)
+ *   .filterBy('parsed.domain', 'example.com')
+ * ```
+ * 
+ * @example Retrieve an array of all redirects for each URL
+ * ```
+ * const q = new Query(redirects)
+ *   .return({ name: 'requested_url', path: 'url')
+ *   .return({ name: 'redirect_list', document: 'rw', path: 'redirects' })
+ * ```
+ * 
+ * @example Retrieve redirect chains that ended in an error
+ * ```
+ * const q = new Query(redirects)
+ *   .filterBy({ document: 'rs', path: 'code', eq: 200, negate: true })
+ * ```
+ */
 export const redirects: AqQuery = {
   collection: 'unique_urls',
   document: 'uu',
@@ -40,15 +75,41 @@ export const redirects: AqQuery = {
     }
   ],
   filters: [
-    { path: 'parsed.protocol', in: ['https:', 'http:'] },
     { path: 'parsed.href', eq: 'rs.parsed.href', value: 'dynamic', negate: true },
     { document: 'rw', path: 'redirects', gt: 1, function: 'count' },
   ],
-  return: [
-    { name: 'requested', document: 'uu', path: 'url' },
-    { name: 'redirects', document: 'rw', path: 'redirects' },
-    { name: 'redirectCount', document: 'rw', path: 'redirects', function: 'count' },
-    { name: 'received', document: 'rs', path: 'url' },
-    { name: 'status', document: 'rs', path: 'code' }
-  ]
-}
+};
+
+
+export const resource_inlinks: AqQuery = {
+  collection: 'responds_with',
+  document: 'rw',
+  filters: [{ path: '_to', eq: 'item._id', value: 'dynamic' }],
+  subqueries: [
+    {
+      collection: 'links_to',
+      document: 'lt',
+      filters: [{ path: '_to', eq: 'rw._from', value: 'dynamic' }],
+      subqueries: [
+        {
+          collection: 'resources',
+          document: 'source',
+          filters: [{ path: '_id', eq: 'lt._from', value: 'dynamic' }],
+        },
+      ],
+    }
+  ],
+};
+
+export const resource_outlinks: AqQuery = {
+  collection: 'links_to',
+  document: 'lt',
+  filters: [{ path: '_from', eq: 'item._id', value: 'dynamic' }],
+  subqueries: [
+    {
+      collection: 'unique_urls',
+      document: 'target',
+      filters: [{ path: '_id', eq: 'lt._to', value: 'dynamic' }],
+    }
+  ],
+};
