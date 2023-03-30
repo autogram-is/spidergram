a7d09550988c19732cdb738b2c56ce76b4e5b3dd
Jeff Eaton <jeff@eaton.fyi>
2023-03-30 13:09:47 -0500

    Transitioning to simpler ESM oclif execution scripts

--- bin/run.js
+++ bin/run.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
-
-import oclif from '@oclif/core'
-
-oclif
-  .run(process.argv.slice(2), import.meta.url)
-  .then(oclif.flush)
-  .catch(oclif.Errors.handle)
+// eslint-disable-next-line node/shebang
+(async () => {
+  const oclif = await import('@oclif/core')
+  await oclif.execute({type: 'esm', dir: import.meta.url})
+})()
