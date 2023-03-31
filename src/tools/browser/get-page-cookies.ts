c0f764a2d6a247ff2883f586e43c96c973102322
Jeff Eaton <jeff@eaton.fyi>
2023-03-31 13:53:29 -0500

    Generalized cookie-grabbin' helper function

---
+++ src/tools/browser/get-page-cookies.ts
@@ -0,0 +1,6 @@
+import { Cookie, Page } from "playwright";
+
+export async function getPageCookies(page: Page) {
+  return page.context().cookies()
+    .then(output => { return { cookies: output } as Record<string, Cookie[]> })
+}
