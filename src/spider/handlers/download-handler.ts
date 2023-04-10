129dadd3a032cecc890a7860f89a9ac850152a96
Jeff Eaton <jeff@eaton.fyi>
2023-04-10 10:20:35 -0500

    Taking a stab at improved error handling for fetched downloads

--- src/spider/handlers/download-handler.ts
+++ src/spider/handlers/download-handler.ts
@@ -30,9 +30,23 @@ export async function downloadHandler(context: SpiderContext): Promise<void> {
       path.join(proj.config.storageDirectory ?? './storage', directory),
     );
     const fullPath = path.join(directory, fileName);
-    await files().writeStream(fullPath, Duplex.from(response.body));
+    const results = await files().writeStream(fullPath, Duplex.from(response.body))
+      .then(() => ({ bucket: 'downloads', path: fullPath }))
+      .catch((error: unknown) => {
+        if (error instanceof Error) {
+          return { code: -1, message: `${error.name} ${error.message}` };
+        } else {
+          return { code: -1, message: undefined, error };
+        }
+      });
 
-    resource.payload = { bucket: 'downloads', path: fullPath };
+    if ('code' in results) {
+      resource.code = results.code;
+      resource.message = results.message ?? '';
+      resource.error = results.error;
+    } else {
+      resource.payload = results;
+    }
     await graph.push(resource);
   }
   return Promise.resolve();
