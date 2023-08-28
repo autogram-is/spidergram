6e127bac3c47f120697fa9f7f5a5928006606748
Jeff Eaton <jeff@eaton.fyi>
2023-08-28 11:25:37 -0500

    Expand Page Markup function notes

--- src/tools/browser/get-page-markup.ts
+++ src/tools/browser/get-page-markup.ts
@@ -2,6 +2,7 @@ import { Page } from 'playwright';
 
 /**
  * Iterates through the document, loading the innerHTML of Shadow Dom elements.
+ * Note that this does not *recursively* load shadow dom elements; that's a TODO.
  * 
  * @see {@link https://docs.apify.com/academy/node-js/scraping-shadow-doms}
  */
