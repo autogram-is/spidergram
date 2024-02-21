d086757e726cce1ae3e27c9e141bbac8687a02b0
Jeff Eaton <jeff@eaton.fyi>
2024-02-21 10:00:58 -0600

    Exporting a keyed version of the charts for future use

--- src/reports/charts/index.ts
+++ src/reports/charts/index.ts
@@ -4,3 +4,19 @@ export * from './line-chart.js';
 export * from './bar-chart.js';
 export * from './donut-chart.js';
 export * from './scatter-plot.js';
+
+import {VegaChart} from './vega.js';
+import {VegaLiteChart} from './vega-lite.js';
+import {LineChart} from './line-chart.js';
+import {BarChart} from './bar-chart.js';
+import {DonutChart} from './donut-chart.js';
+import {ScatterPlot} from './scatter-plot.js';
+
+export const charts = {
+  vega: VegaChart,
+  vegaLite: VegaLiteChart,
+  line: LineChart,
+  bar: BarChart,
+  donut: DonutChart,
+  scatter: ScatterPlot
+}
