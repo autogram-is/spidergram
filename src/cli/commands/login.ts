6529cdc5911619f7ae32bf7331f5e3a472cd58d5
Jeff Eaton <jeff@eaton.fyi>
2024-06-27 12:12:12 -0500

    add `spidergram login <url>`, which opens a browser window, allows you to log in, then saves the cookies for spidering.

---
+++ src/cli/commands/login.ts
@@ -0,0 +1,47 @@
+import { Args } from '@oclif/core';
+import {
+  Spidergram,
+  SgCommand,
+} from '../../index.js';
+import { launchPlaywright } from 'crawlee';
+
+export default class Ping extends SgCommand {
+  static summary = 'Log in to a site and save the resulting cookies';
+
+  static usage = '<%= config.bin %> <%= command.id %> [url]';
+
+  static args = {
+    url: Args.url({
+      description: 'A valid URL with a standard login form',
+      required: true,
+    }),
+  };
+
+  static strict = false;
+
+  async run() {
+    const sg = await Spidergram.load();
+    const { args } = await this.parse(Ping);
+
+    const browser = await launchPlaywright({ launchOptions: { headless: false }});
+    const browserContext = browser.contexts()[0];
+    const page = await browserContext.newPage();
+
+    this.log('Log in, then return to the terminal.');
+
+    await page.goto(args.url.toString(), {
+      waitUntil: sg.config.spider?.waitUntil,
+    });
+
+    await this.ux.anykey('Press any key here AFTER logging in');
+
+    const cookies = await page.context().cookies();
+
+    this.ux.styledObject(cookies);
+
+    await page.close();
+    await browser.close();
+
+    sg.files('output').write('cookies.json', Buffer.from(JSON.stringify({ spider: { cookies: cookies } }, undefined, 2)));
+  }
+}
